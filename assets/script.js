6c7b69c4b3be5965e2f7c7a327c0afca2146c7c6
Alissa Champigny <achampigny4@gmail.com>
2020-07-14 21:45:21 -0400

    info class style added

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+// quiz questions and choices
+// assign choices to buttons A-D
+// assign correct to correct answers
+// assign wrong to inccorect answers
+// "quiz question" get replaced with the actual question once start button clicked
+// buttons A-D get replaced by answers when start button is clicked
+// function that switches to the next question after anwer is selected
+// timer that starts when start button is clicked
+// start button dispears after onClick event???
+// timer counts down
+// time gets removed from timer when answer is wrong
+// timer gets to zero score is shown below timer
+// promt to add user initials
+// score and user initials gets saved to local storage
+// a link to another page to show top 5 scores below game score
+
+
+}
